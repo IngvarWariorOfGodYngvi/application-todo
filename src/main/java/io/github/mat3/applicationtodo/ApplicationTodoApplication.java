525401621b1e41bc5a6a8397936e5691a1b4a5f1
izebr <i.zebrowski.ul@gmail.com>
2020-10-27 18:10:10 +0100

    initial commit

---
+++ src/main/java/io/github/mat3/applicationtodo/ApplicationTodoApplication.java
@@ -0,0 +1,15 @@
+package io.github.mat3.applicationtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTodoApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/io/github/mat3/applicationtodo/ApplicationTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mat3.applicationtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
