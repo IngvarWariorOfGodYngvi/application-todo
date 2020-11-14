97d77eaf13b28735748f8d8a1bf53615afa3b1fb
izebr <i.zebrowski.ul@gmail.com>
2020-11-14 18:27:20 +0100

    lesson 37 done

--- src/main/java/io/github/mat3/applicationtodo/model/TaskRepository.java
+++ src/main/java/io/github/mat3/applicationtodo/model/TaskRepository.java
@@ -11,7 +11,7 @@ public interface TaskRepository {
     List<Task> findAll();
     Page<Task> findAll(Pageable page);
     Optional<Task> findById(Integer id);
-    Task save(Task entity);
     List<Task> findByDone(@Param("state") boolean done);
+    Task save(Task entity);
 
 }
