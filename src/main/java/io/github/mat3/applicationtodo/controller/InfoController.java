4093685bb3536b6e99dfb906e92fb80159554574
izebr <i.zebrowski.ul@gmail.com>
2020-11-14 20:23:49 +0100

    lesson 47 done

--- src/main/java/io/github/mat3/applicationtodo/controller/InfoController.java
+++ src/main/java/io/github/mat3/applicationtodo/controller/InfoController.java
@@ -1,22 +1,25 @@
 package io.github.mat3.applicationtodo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
-@Value("${spring.datasource.url}")
-    private String url;
-@Value("${my.prop}")
+    @Autowired
+    private DataSourceProperties dataSource;
+    @Value("${my.prop}")
     private String myProp;
 
-    @GetMapping("/info/url")
-    String ulr(){
-        return url;
+    @GetMapping("/info/dataSource")
+    String ulr() {
+        return dataSource.getUrl();
     }
+
     @GetMapping("/info/prop")
-    String myProp(){
+    String myProp() {
         return myProp;
     }
 
