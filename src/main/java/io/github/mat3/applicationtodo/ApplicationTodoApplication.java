b561674f2094d1f8c59fc53707e08aa93129c6e2
izebr <i.zebrowski.ul@gmail.com>
2020-11-14 17:40:40 +0100

    lesson 35 done

--- src/main/java/io/github/mat3/applicationtodo/ApplicationTodoApplication.java
+++ src/main/java/io/github/mat3/applicationtodo/ApplicationTodoApplication.java
@@ -2,14 +2,20 @@ package io.github.mat3.applicationtodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class ApplicationTodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationTodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTodoApplication.class, args);
+    }
 
-
+    @Bean
+    Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
 
 }
