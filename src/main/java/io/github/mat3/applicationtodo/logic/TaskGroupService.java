1b84a049aff03b120eeacda37c456775f6115b07
izebr <i.zebrowski.ul@gmail.com>
2020-11-20 00:49:21 +0100

    lesson 91 done

--- src/main/java/io/github/mat3/applicationtodo/logic/TaskGroupService.java
+++ src/main/java/io/github/mat3/applicationtodo/logic/TaskGroupService.java
@@ -2,6 +2,7 @@ package io.github.mat3.applicationtodo.logic;
 
 import io.github.mat3.applicationtodo.model.TaskGroup;
 import io.github.mat3.applicationtodo.model.TaskGroupRepository;
+import io.github.mat3.applicationtodo.model.TaskRepository;
 import io.github.mat3.applicationtodo.model.projection.GroupReadModel;
 import io.github.mat3.applicationtodo.model.projection.GroupWriteModel;
 import org.springframework.stereotype.Service;
@@ -12,20 +13,32 @@ import java.util.stream.Collectors;
 @Service
 public class TaskGroupService {
     private TaskGroupRepository repository;
+    private TaskRepository taskRepository;
 
-    TaskGroupService(final TaskGroupRepository repository) {
+    TaskGroupService(final TaskGroupRepository repository, TaskRepository taskRepository) {
         this.repository = repository;
+        this.taskRepository = taskRepository;
     }
 
-    public GroupReadModel createGroup(GroupWriteModel source){
+    public GroupReadModel createGroup(GroupWriteModel source) {
         TaskGroup result = repository.save(source.toGroup());
         return new GroupReadModel(result);
     }
 
-    public List<GroupReadModel> readAll(){
+    public List<GroupReadModel> readAll() {
         return repository.findAll()
                 .stream()
                 .map(GroupReadModel::new)
                 .collect(Collectors.toList());
+    }
+
+    public void toggleGroup(int groupId) {
+        if (taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)) {
+            throw new IllegalStateException("Group has undone tasks. Done all the tasks first");
+        }
+        TaskGroup result = repository.findById(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("TaskGroup with given ID not found"));
+        result.setDone(!result.isDone());
+
     }
 }
