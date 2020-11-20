0a57cab37b26c8a03fb9235e3e6177d024b7f03a
izebr <i.zebrowski.ul@gmail.com>
2020-11-20 01:30:27 +0100

    lesson 95 done

---
+++ src/test/java/io/github/mat3/applicationtodo/logic/ProjectServiceTest.java
@@ -0,0 +1,12 @@
+package io.github.mat3.applicationtodo.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @Test
+    void createGroup() {
+    }
+}
