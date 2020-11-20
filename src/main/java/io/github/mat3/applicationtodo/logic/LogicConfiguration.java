0a1b634962f15fc85a02d5ae67685657aafda654
izebr <i.zebrowski.ul@gmail.com>
2020-11-20 22:15:47 +0100

    lesson 117 done

--- src/main/java/io/github/mat3/applicationtodo/logic/LogicConfiguration.java
+++ src/main/java/io/github/mat3/applicationtodo/logic/LogicConfiguration.java
@@ -3,16 +3,20 @@ package io.github.mat3.applicationtodo.logic;
 import io.github.mat3.applicationtodo.TaskConfigurationProperties;
 import io.github.mat3.applicationtodo.model.ProjectRepository;
 import io.github.mat3.applicationtodo.model.TaskGroupRepository;
+import io.github.mat3.applicationtodo.model.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("classpath:applicationContext.xml")
 class LogicConfiguration {
-    
+
     @Bean
     ProjectService service(ProjectRepository repository, TaskGroupRepository taskGroupRepository, TaskConfigurationProperties config) {
         return new ProjectService(repository, taskGroupRepository, config);
+    }
+
+    @Bean
+    TaskGroupService taskGroupService(TaskGroupRepository taskGroupRepository, TaskRepository taskRepository) {
+        return new TaskGroupService(taskGroupRepository, taskRepository);
     }
 }
