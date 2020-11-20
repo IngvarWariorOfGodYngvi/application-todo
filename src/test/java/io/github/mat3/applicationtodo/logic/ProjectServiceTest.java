1d7ce8073913915887c8874a26e09953add3527c
izebr <i.zebrowski.ul@gmail.com>
2020-11-20 01:41:47 +0100

    lesson 96 done

--- src/test/java/io/github/mat3/applicationtodo/logic/ProjectServiceTest.java
+++ src/test/java/io/github/mat3/applicationtodo/logic/ProjectServiceTest.java
@@ -1,12 +1,24 @@
 package io.github.mat3.applicationtodo.logic;
 
+import io.github.mat3.applicationtodo.model.TaskGroup;
+import io.github.mat3.applicationtodo.model.TaskGroupRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectServiceTest {
 
     @Test
-    void createGroup() {
+    @DisplayName("should throw IllegalStateException when configured to allow just 1 group the other undone group exists")
+    void createGroup_noMultipleGroupsConfig_And_undoneGroupExists_throwIllegalStateException() {
+        //given
+        var mockGroupRepository = 
+        }
+        //when
+        //then
     }
 }
