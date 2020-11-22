1712f49e76391303e8ff22a142c1b30c8d15c369
izebr <i.zebrowski.ul@gmail.com>
2020-11-22 21:06:10 +0100

    lesson 137 done

--- src/main/java/io/github/mat3/applicationtodo/controller/LoggerInterceptor.java
+++ src/main/java/io/github/mat3/applicationtodo/controller/LoggerInterceptor.java
@@ -2,18 +2,21 @@ package io.github.mat3.applicationtodo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class LoggerInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        logger.info("[Interceptor] " +request.getMethod() + " " + request.getRequestURI());
+        logger.info("[preHandle] " + request.getMethod() + " " + request.getRequestURI());
         return true;
     }
 }
