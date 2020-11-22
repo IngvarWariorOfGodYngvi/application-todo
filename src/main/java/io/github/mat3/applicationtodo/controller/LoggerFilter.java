c96bc5c7fe4a01172b6f6ebc73b2fd6a7cd23f63
izebr <i.zebrowski.ul@gmail.com>
2020-11-22 20:28:59 +0100

    lesson 136 done

---
+++ src/main/java/io/github/mat3/applicationtodo/controller/LoggerFilter.java
@@ -0,0 +1,25 @@
+package io.github.mat3.applicationtodo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class LoggerFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if(request instanceof HttpServletRequest){
+            var httpServletRequest = (HttpServletRequest) request;
+            logger.info("[doFilter] " + httpServletRequest.getMethod() + " " +httpServletRequest.getRequestURI());
+        }
+        //jeśli nie dam chain to filtrowanie popsuje działanie programu
+        chain.doFilter(request,response);
+    }
+
+}
