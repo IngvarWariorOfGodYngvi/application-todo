0d31a6d63f540ee45617ca375deb979e9bc6b87b
izebr <i.zebrowski.ul@gmail.com>
2020-11-24 18:21:12 +0100

    lesson 162 done

--- src/main/java/io/github/mat3/applicationtodo/controller/ProjectController.java
+++ src/main/java/io/github/mat3/applicationtodo/controller/ProjectController.java
@@ -1,14 +1,19 @@
 package io.github.mat3.applicationtodo.controller;
 
 import io.github.mat3.applicationtodo.logic.ProjectService;
+import io.github.mat3.applicationtodo.model.Project;
 import io.github.mat3.applicationtodo.model.ProjectSteps;
 import io.github.mat3.applicationtodo.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/projects")
@@ -24,8 +29,15 @@ public class ProjectController {
         model.addAttribute("project", new ProjectWriteModel());
         return "projects";
     }
+
     @PostMapping
-    String addProject(@ModelAttribute("project") ProjectWriteModel current,Model model){
+    String addProject(
+            @ModelAttribute("project") @Valid ProjectWriteModel current,
+            BindingResult bindingResult,
+            Model model) {
+        if(bindingResult.hasErrors()){
+            return "projects";
+        }
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
         model.addAttribute("message", "Dodano Projekt!");
@@ -33,9 +45,14 @@ public class ProjectController {
     }
 
     @PostMapping(params = "addStep")
-    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current){
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectSteps());
         return "projects";
+    }
+
+    @ModelAttribute("projects")
+    List<Project> getProjects() {
+        return service.readAll();
     }
 
 }
