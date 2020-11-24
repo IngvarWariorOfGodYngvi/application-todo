47acc483e281b93b90134be071274f0a327c1092
izebr <i.zebrowski.ul@gmail.com>
2020-11-24 14:25:11 +0100

    lesson 158 done

--- src/main/java/io/github/mat3/applicationtodo/controller/ProjectController.java
+++ src/main/java/io/github/mat3/applicationtodo/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package io.github.mat3.applicationtodo.controller;
 
+import io.github.mat3.applicationtodo.logic.ProjectService;
 import io.github.mat3.applicationtodo.model.ProjectSteps;
 import io.github.mat3.applicationtodo.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
@@ -12,10 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectService service;
+
+    public ProjectController(ProjectService service) {
+        this.service = service;
+    }
 
     @GetMapping
     String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel current,Model model){
+        service.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano Projekt!");
         return "projects";
     }
 
