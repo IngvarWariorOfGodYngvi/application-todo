1c0587db02c68d918bb0c323338430a78976ea93
izebr <i.zebrowski.ul@gmail.com>
2020-11-24 02:45:17 +0100

    lesson 153 done

--- src/main/java/io/github/mat3/applicationtodo/controller/ProjectController.java
+++ src/main/java/io/github/mat3/applicationtodo/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package io.github.mat3.applicationtodo.controller;
 
+import io.github.mat3.applicationtodo.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectController {
 
     @GetMapping
-    String showProjects() {
+    String showProjects(Model model) {
+        ProjectWriteModel projectToEdit = new ProjectWriteModel();
+        projectToEdit.setDescription("test");
+        model.addAttribute("project", projectToEdit);
         return "projects";
     }
 
