8a364d1705600f021409704611d466ed86fc9817
izebr <i.zebrowski.ul@gmail.com>
2020-11-25 15:26:36 +0100

    lesson 186 done

--- src/main/java/io/github/mat3/applicationtodo/controller/LogicAspect.java
+++ src/main/java/io/github/mat3/applicationtodo/controller/LogicAspect.java
@@ -2,22 +2,37 @@ package io.github.mat3.applicationtodo.controller;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogicAspect {
-
+    public static final Logger logger = LoggerFactory.getLogger(LogicAspect.class);
     private final Timer projectCreateGroupTimer;
 
     public LogicAspect(final MeterRegistry registry) {
-        projectCreateGroupTimer = registry.timer("logic.product.create.group");
+        projectCreateGroupTimer = registry.timer("logic.project.create.group");
     }
 
-    @Around("execution(* io.github.mat3.applicationtodo.logic.ProjectService.createGroup(..))")
+    @Pointcut("execution(* io.github.mat3.applicationtodo.logic.ProjectService.createGroup(..))")
+    static void projectServiceCreateGroup() {
+
+    }
+
+    @Before("projectServiceCreateGroup()")
+    void logMethodCall(JoinPoint jp) {
+        logger.info("Before {} with {}", jp.getSignature().getName(), jp.getArgs());
+    }
+
+    @Around("projectServiceCreateGroup()")
     Object aroundProjectCreateGroup(ProceedingJoinPoint jp) {
         return projectCreateGroupTimer.record(() -> {
             try {
