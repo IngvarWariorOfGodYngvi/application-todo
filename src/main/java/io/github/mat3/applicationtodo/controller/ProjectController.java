a668e128a2a9acff38632d12de7f06fb381c4a9a
izebr <i.zebrowski.ul@gmail.com>
2020-11-25 12:35:30 +0100

    lesson 164 done

--- src/main/java/io/github/mat3/applicationtodo/controller/ProjectController.java
+++ src/main/java/io/github/mat3/applicationtodo/controller/ProjectController.java
@@ -4,15 +4,14 @@ import io.github.mat3.applicationtodo.logic.ProjectService;
 import io.github.mat3.applicationtodo.model.Project;
 import io.github.mat3.applicationtodo.model.ProjectSteps;
 import io.github.mat3.applicationtodo.model.projection.ProjectWriteModel;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -47,6 +46,22 @@ public class ProjectController {
     @PostMapping(params = "addStep")
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectSteps());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline
+    ) {
+        try {
+            service.createGroup(deadline, id);
+            model.addAttribute("message", "Dodano grupę");
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            model.addAttribute("message", "Błąd podczas tworzenia grupy!");
+        }
         return "projects";
     }
 
