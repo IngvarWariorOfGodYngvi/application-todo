74825504df378857f4078e16deef0a14bcfcd29d
izebr <i.zebrowski.ul@gmail.com>
2020-11-26 21:41:13 +0100

    lesson 195 before app start

--- src/main/java/io/github/mat3/applicationtodo/reports/ChangedTaskEventListener.java
+++ src/main/java/io/github/mat3/applicationtodo/reports/ChangedTaskEventListener.java
@@ -1,6 +1,7 @@
 package io.github.mat3.applicationtodo.reports;
 
 import io.github.mat3.applicationtodo.model.event.TaskDone;
+import io.github.mat3.applicationtodo.model.event.TaskEvent;
 import io.github.mat3.applicationtodo.model.event.TaskUndone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +12,27 @@ import org.springframework.stereotype.Service;
 class ChangedTaskEventListener {
     private static final Logger logger = LoggerFactory.getLogger(ChangedTaskEventListener.class);
 
+    private final PersistedTaskEventRepository repository;
+
+    ChangedTaskEventListener(PersistedTaskEventRepository repository) {
+        this.repository = repository;
+    }
+
+
     @EventListener
     public void on(TaskDone event) {
-        logger.info("Got " + event);
+        onChanged(event);
     }
+
 
     @EventListener
     public void on(TaskUndone event) {
+        onChanged(event);
+
+    }
+
+    private void onChanged(TaskEvent event) {
         logger.info("Got " + event);
+        repository.save(new PersistedTaskEvent(event));
     }
 }
